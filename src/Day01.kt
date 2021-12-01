f04171dfcfd38f16884f880d25f781b339f277b4
Ondrej Ruttkay <ruttkay@gmail.com>
2021-12-01 18:40:07 +0100

    Add day 1 solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,30 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var increases = 0
+        for (i in 0 until input.size - 1) {
+            val before = Integer.parseInt(input[i])
+            val current = Integer.parseInt(input[i+1])
+            if (before < current) increases++
+        }
+        return increases
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var increases = 0
+        for (i in 0 until input.size - 3) {
+            val a = Integer.parseInt(input[i])
+            val b = Integer.parseInt(input[i+1])
+            val c = Integer.parseInt(input[i+2])
+            val d = Integer.parseInt(input[i+3])
+            val before = a + b + c
+            val current = b + c + d
+            if (before < current) increases++
+        }
+        return increases
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input1 = readInput("Day01part1")
+    val input2 = readInput("Day01part2")
+    println(part1(input1))
+    println(part2(input2))
 }
